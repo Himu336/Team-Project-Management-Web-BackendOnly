1a147251bb968391645c85ed33dac70ddb2a94ad
Himansh <himanshbansal200521@gmail.com>
2025-03-31 00:14:23 +0530

    created project endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import userRoutes from "./routes/user.routes";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.routes";
+import projectRoutes from "./routes/project.routes";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -55,6 +56,7 @@ app.use(`${BASE_PATH}/auth` , authRoutes);
 app.use(`${BASE_PATH}/user` , isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace` , isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member` , isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project` , isAuthenticated, projectRoutes);
 
 app.use(errorHandler);
 
